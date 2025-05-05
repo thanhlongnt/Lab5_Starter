92ea236098a0f3c6e396d4b9501e47676082800b
Thanh-Long Nguyen-Trong <thanhlong.nguyentrong@gmail.com>
2025-05-04 22:57:05 -0700

    skeleton added explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,5 +1,15 @@
 // explore.js
 
+// select
+// use api to select correct audio files for each dropdown
+
+
+
+// push to talk
+// use api to text to speech
+
+
+
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
