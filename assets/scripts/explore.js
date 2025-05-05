a936f3ead03a69b6609254021ef197bdf25da38b
Thanh-Long Nguyen-Trong <thanhlong.nguyentrong@gmail.com>
2025-05-04 23:08:16 -0700

    added push to talk feature

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -8,10 +8,20 @@
 // push to talk
 // use api to text to speech
 
+function pushToTalk() {
+  document.querySelector("button").addEventListener("click", function () {
+      const text = document.getElementById("text-to-speak").value;
+      console.log(text);
 
+      const utterance = new SpeechSynthesisUtterance(text);
+      speechSynthesis.speak(utterance);
+    }, false
+  );
+}
 
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  pushToTalk();
 }
