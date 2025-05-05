185968c63d3d6906db8e65b79cefb900080ed1d6
Thanh-Long Nguyen-Trong <thanhlong.nguyentrong@gmail.com>
2025-05-05 00:27:35 -0700

    added tests for phone and email

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -11,11 +11,39 @@ import {
 // TODO - Part 2
 
 // isPhoneNumber
+test('Is Valid Phone: Parenthesis', () => {
+  expect(isPhoneNumber("(510)-990-4632")).toBe(true);
+  expect(isPhoneNumber("408-999-5678")).toBe(true);
+});
 
+test('Is Valid Phone: No Parenthesis', () => {
+  expect(isPhoneNumber("408-999-5678")).toBe(true);
+});
 
+test('Is not valid phone num: no formating', () => {
+  expect(isPhoneNumber("5109904632")).toBe(false);
+});
+
+test('Is not valid phone num: missing format', () => {
+  expect(isPhoneNumber("(510)-9904632")).toBe(false);
+});
 
 // isEmail
+test('Is valid email: @ucsd.edu domain', () => {
+  expect(isEmail("tnguyentrong@ucsd.edu")).toBe(true);
+});
 
+test('Is valid email: @gmail.com domain', () => {
+  expect(isEmail("natejohnson234@gmail.com")).toBe(true);
+});
+
+test('Is not valid email: missing domain', () => {
+  expect(isEmail("natejohnson234@")).toBe(false);
+});
+
+test('Is not valid email: no prefix', () => {
+  expect(isEmail("@gmail.com")).toBe(false);
+});
 
 
 // isStrongPassword
