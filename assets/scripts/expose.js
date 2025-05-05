cd4e068644609b8f3cdbeb254bff6a20d7219429
William Widjaja <wwidjaja0@gmail.com>
2025-05-04 21:45:26 -0700

    drop-down changes

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,7 +2,27 @@
 
 // selecting a horn
 
-// TODO
+function selectHorn() {
+  document.getElementById("horn-select").addEventListener("input", function (event) {
+    const hornSelect = document.getElementById("horn-select");
+    const hornImage = document.querySelector("img");
+    const hornAudio = document.querySelector("audio");
+    
+    if (hornSelect.value === "air-horn") {
+      hornImage.src = "./assets/images/air-horn.svg";
+      hornAudio.src = "./assets/audio/air-horn.mp3";
+    } else if (hornSelect.value === "car-horn") {
+      hornImage.src = "./assets/images/car-horn.svg";
+      hornAudio.src = "./assets/audio/car-horn.mp3";
+    } else if (hornSelect.value === "party-horn") {
+      hornImage.src = "./assets/images/party-horn.svg";
+      hornAudio.src = "./assets/audio/party-horn.mp3";
+    } else {
+      hornImage.src = "./assets/images/no-image.png";
+      hornAudio.src = "";
+    }
+  }, false);
+}
 
 // change volume on slider
 
