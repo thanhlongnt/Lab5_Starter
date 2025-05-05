7f0c42e7caf8ad6037453b0fdf4024b28aaafad1
Thanh-Long Nguyen-Trong <thanhlong.nguyentrong@gmail.com>
2025-05-04 21:54:27 -0700

    added functional volume switch. bad formating

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -8,6 +8,31 @@
 
 // TODO
 
+const volumeSlider = document.getElementById('volume');
+const volumeImg = document.querySelector("#volume-controls img");
+
+volumeSlider.addEventListener('input', () => {
+  console.log(volumeSlider.value);
+
+  if (volumeSlider.value == 0) {
+    volumeImg.src = "assets/icons/volume-level-0.svg";
+  } 
+  
+  else if ( 1 < volumeSlider.value && volumeSlider.value < 33) {
+    volumeImg.src = "assets/icons/volume-level-1.svg";
+  } 
+  
+  else if (33 < volumeSlider.value && volumeSlider.value < 67) {
+    volumeImg.src = "assets/icons/volume-level-2.svg";
+  } 
+  
+  else {
+    volumeImg.src = "assets/icons/volume-level-3.svg";
+  }
+
+});
+
+
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
