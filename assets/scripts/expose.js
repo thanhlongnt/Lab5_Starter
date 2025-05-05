03c3e36b87c28bb2e5b3a29623ac401c7151e431
Thanh-Long Nguyen-Trong <thanhlong.nguyentrong@gmail.com>
2025-05-04 22:18:15 -0700

    cleaned up volume slider

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -32,26 +32,20 @@ document.getElementById('volume').addEventListener("input", selectVolume);
 function selectVolume() {
   const volumeSlider = document.getElementById('volume')
   const volumeImg = document.querySelector("#volume-controls img");
-  volumeSlider.addEventListener('input', function(event) {
-    console.log(volumeSlider.value);
+  console.log(volumeSlider.value);
   
-    if (volumeSlider.value == 0) {
-      volumeImg.src = "assets/icons/volume-level-0.svg";
-    } 
-    
-    else if ( 1 < volumeSlider.value && volumeSlider.value < 33) {
-      volumeImg.src = "assets/icons/volume-level-1.svg";
-    } 
-    
-    else if (33 < volumeSlider.value && volumeSlider.value < 67) {
-      volumeImg.src = "assets/icons/volume-level-2.svg";
-    } 
-    
-    else {
-      volumeImg.src = "assets/icons/volume-level-3.svg";
-    }
-  
-  });
+  if (volumeSlider.value == 0) {
+    volumeImg.src = "assets/icons/volume-level-0.svg";
+  } 
+  else if ( 1 < volumeSlider.value && volumeSlider.value < 33) {
+    volumeImg.src = "assets/icons/volume-level-1.svg";
+  } 
+  else if (33 < volumeSlider.value && volumeSlider.value < 67) {
+    volumeImg.src = "assets/icons/volume-level-2.svg";
+  } 
+  else {
+    volumeImg.src = "assets/icons/volume-level-3.svg";
+  }
 }
 
 window.addEventListener('DOMContentLoaded', init);
